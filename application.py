a00ee21090e36b5954302509b5f008df2881410b
BlueTurtle121 <tfraser02@gmail.com>
2019-12-15 01:15:34 +1000

    Skeleton

--- application.py
+++ application.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os 
+
 app = Flask(__name__)
+app.secret_key='yeehawdingdangdiddly'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///joystik.sqlite'
+app.config[SQLALCHEMY_DATABASE_URI] = os.environ['DATABASE_URL']
+app.register_error_handler(404, pagenotfound)
+db.init_app(app)
+app.run(debug=True)
 
 @app.route('/')
-def start():
-    return '<h1>Placeholder 1</h1>'
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Other routes
+@app.errorhandler(404)
+def pagenotfound(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(401)
+def forbidden(e):
+    return render_template('401.html'), 401
+
+@app.errorhandler(500)
+def servererror(e):
+    return render_template('500.html'), 500
