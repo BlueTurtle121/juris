e18e343abadd218d4fd094f08f4eab5897a1a748
BlueTurtle121 <tfraser02@gmail.com>
2019-12-15 01:33:12 +1000

    please work?

--- application.py
+++ application.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 import os 
 
 app = Flask(__name__)
-app.secret_key='yeehawdingdangdiddly'
-app.run(debug=True)
 
 @app.route('/')
 def index():
