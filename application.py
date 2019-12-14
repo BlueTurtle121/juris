bc14a66f600859eeaff3fef7f9d5e9d8c7540cfd
BlueTurtle121 <tfraser02@gmail.com>
2019-12-15 01:18:29 +1000

    remove database

--- application.py
+++ application.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 import os 
 
 app = Flask(__name__)
 app.secret_key='yeehawdingdangdiddly'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///joystik.sqlite'
-app.config[SQLALCHEMY_DATABASE_URI] = os.environ['DATABASE_URL']
 app.register_error_handler(404, pagenotfound)
 db.init_app(app)
 app.run(debug=True)
